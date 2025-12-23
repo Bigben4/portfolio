4b6ef49d353a9a8035d803ca517fc3a62206a510
ben <ebrahimndille@gmail.com>
2025-12-22 16:14:44 -0800

    adding helpers to the scene

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,6 +1,7 @@
 
 import './style.css';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 // Scene container for all objects
 const scene = new THREE.Scene();
@@ -25,9 +26,28 @@ camera.position.z = 30;
 
 // Add a torus to the scene
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({ color: 0xff6347, wireframe: true });
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
 const torus = new THREE.Mesh(geometry, material);
 scene.add(torus);
+
+// adding light
+const pointLight = new THREE.PointLight(0xffffff, 1);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, 1); // soft ambient light
+scene.add( pointLight, ambientLight);
+ 
+
+// adding helper
+const lighthelper = new THREE.PointLightHelper(pointLight,1);
+const gridhelper = new THREE.GridHelper(200, 50);
+scene.add(lighthelper, gridhelper)
+
+
+// orbit controls
+const controll = new OrbitControls(camera, renderer.domElement);
+
+
 
 // Handle resize
 window.addEventListener('resize', () => {
@@ -43,10 +63,13 @@ function animate() {
     torus.rotation.x += 0.01;
     torus.rotation.y += 0.005;
     torus.rotation.z += 0.01;
-
+    controll.update();
     renderer.render(scene, camera);
 }
 
 animate();
+
+
+
 
 
